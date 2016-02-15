31b5b34415ef7edd240f785d67ef1de580eee579
Leo Gopal <leo@leogopal.com>
2016-02-15 15:44:50 +0200

    Enables cookie.js cookie handling.
    Plugin complete.

--- assets/js/frontgate.js
+++ assets/js/frontgate.js
@@ -6,10 +6,24 @@ jQuery(document).ready( function ($) {
 
 
 	function initialize_age_gate() {
+		/**
+		 * Adds a class to the body to disallow scrolling.
+		 */
 		$("body").addClass("overlay-active");
+
+		/**
+		 * Brings the overlay into view.
+		 */
 		$("#nona-overlay-wrap").fadeIn(500);
+
+		/**
+		 * Creates a responsive full background image for the Div.
+		 */
 		$("#nona-overlay-wrap").backstretch(backstretch_image);
 
+		/**
+		 * This allows us to auto-tab to next input field on completion.
+		 */
 		$('#nona_verify_d').autotab({
 			format: 'alphanumeric',
 			target: '#nona_verify_m'
@@ -27,18 +41,37 @@ jQuery(document).ready( function ($) {
 		$("#nona_verify").on("click", function(e) {
 
 			e.preventDefault();
-
+			/**
+			 * Checks if the user is_old_enough
+			 */
 			if ( is_old_enough() ) {
-			  //  cookie.set( 'nona-age-verified', 'verified', {
-				//    expires: time_to_remember,
-				//    domain: document.location.hostname,
-				//    path: '/',
-				//    secure: false
-				// });
+				/**
+				 * if the user is old enough, sets cookie.
+				 * The time_to_remember is how long it will last
+				 * path is the root of the domain
+				 * domain is the current host (dynamic)
+				 * secure checks whether https
+				 */
+			  cookie.set( 'nona-age-verified', 'verified', {
+				   expires: time_to_remember,
+				   domain: document.location.hostname,
+				   path: '/',
+				   secure: false
+				});
+
+				/**
+				 * removes the body class to disallow scrolling.
+				 */
 				$("body").removeClass("overlay-active");
+				/**
+				 * Makes the overlay fadeout
+				 */
 				$("#nona-overlay-wrap").fadeOut(500);
 
 			} else {
+				/**
+				 * If user is too young, error message slides into view.
+				 */
 				$("#error-too-young").slideDown().delay(3000).slideUp("slow");
 			}
 
@@ -63,7 +96,7 @@ jQuery(document).ready( function ($) {
 		return (age >= age_to_restrict); //returns boolean
 	}
 
-		// INIT AGE GATE
+	// INIT AGE GATE
 	if( ! cookie.get('nona-age-verified') ) {
 		initialize_age_gate();
 	}
